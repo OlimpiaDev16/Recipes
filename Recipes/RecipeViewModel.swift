b8816bed1432d286cbe62b7797bf512cc505bd3d
OlimpiaDev16 <168941945+OlimpiaDev16@users.noreply.github.com>
2025-02-11 15:57:03 -0500

    Create a viewModel

---
+++ Recipes/RecipeViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  RecipesViewModel.swift
+//  Recipes
+//
+//  Created by Olimpia Compean on 2/11/25.
+//
+
+import Foundation
+import SwiftUI
+
+@MainActor
+class RecipeViewModel: ObservableObject {
+    private let recipeService: RecipeService
+    
+    @Published var recipesWithImages: [RecipeWithImage] = []
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String?
+    
+    init(recipeService: RecipeService) {
+        self.recipeService = recipeService
+    }
+    
+    func fetchRecipes() async {
+        isLoading = true
+        errorMessage = nil
+        do {
+            let recipes = try await recipeService.fetchRecipesAndImages()
+            self.recipesWithImages = recipes
+        } catch {
+            self.errorMessage = "Failed to load recipes: \(error.localizedDescription)"
+        }
+        isLoading = false 
+    }
+}
